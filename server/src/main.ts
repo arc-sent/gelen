1b064fe5eb9e4e0fa04939426ae1ae13bc643b47
arc-sent <amajorov731@gmail.com>
2025-10-24 16:15:32 +0300

    Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,8 +7,9 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://admin:4173', 'http://frontend:5174'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
