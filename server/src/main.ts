c82080f473506653f93bc84a3577844ade5370ac
arc-sent <amajorov731@gmail.com>
2025-10-26 01:38:13 +0300

    UpdateMain

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,14 +12,14 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:5174',
-      "http://85.193.87.111:4173",
+      'http://85.193.87.111:4173',
       'http://85.193.87.111:4174',
       'http://gelotpusk.ru',
       'http://www.gelotpusk.ru',
-      'http://admin.gelotpusk.ru/',
+      'http://admin.gelotpusk.ru',
       'https://gelotpusk.ru',
       'https://www.gelotpusk.ru',
-      'https://admin.gelotpusk.ru/'
+      'https://admin.gelotpusk.ru'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
